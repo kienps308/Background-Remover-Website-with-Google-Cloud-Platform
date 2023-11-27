da55088d953994944d2d5ade7c80bd9d8fb91ae2
Kien Phan <phansykien2k@gmail.com>
2023-11-27 16:28:40 +0000

    test

--- application/auth.py
+++ application/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request,flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user,login_required
 from .models.User import User
-from . import db
+from . import session
 
 auth = Blueprint('auth', __name__)
 
@@ -17,7 +17,7 @@ def login_post():
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
 
-    user = User.query.filter_by(email=email).first()
+    user = session.query(User).filter_by(email=email).first()
 
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
@@ -41,7 +41,7 @@ def register_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    user = session.query(User).filter_by(email=email).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')
@@ -51,8 +51,8 @@ def register_post():
     new_user = User(email=email, name=name, password=generate_password_hash(password))
 
     # add the new user to the database
-    db.session.add(new_user)
-    db.session.commit()
+    session.add(new_user)
+    session.commit()
 
     return redirect(url_for('auth.login'))
 
--- application/models/User.py
+++ application/models/User.py
@@ -1,8 +1,11 @@
-from application import db
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, Column
 from flask_login import UserMixin
 
-class User(UserMixin,db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+base = declarative_base()
+class User(UserMixin, base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = Column(String(), unique=True)
+    password = Column(String())
+    Usname = Column(String())
